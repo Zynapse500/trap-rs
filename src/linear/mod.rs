082dbf9dfaf05a56847e51e1d6c952af62bce94f
Christofer Nolander <christofer.nolander@gmail.com>
2018-02-10 17:49:34 +0100

    Added conversion between floating and integer vectors

--- src/linear/mod.rs
+++ src/linear/mod.rs
@@ -16,3 +16,68 @@ pub use self::vectori::{
 
 mod matrix;
 pub use self::matrix::Matrix4;
+
+
+
+// Conversion between vector types
+impl From<Vector2i> for Vector2 {
+    fn from(v: Vector2i) -> Self {
+        Vector2 {
+            x: v.x as f64,
+            y: v.y as f64
+        }
+    }
+}
+
+impl From<Vector2> for Vector2i {
+    fn from(v: Vector2) -> Self {
+        Vector2i {
+            x: v.x as i64,
+            y: v.y as i64
+        }
+    }
+}
+
+
+impl From<Vector3i> for Vector3 {
+    fn from(v: Vector3i) -> Self {
+        Vector3 {
+            x: v.x as f64,
+            y: v.y as f64,
+            z: v.z as f64,
+        }
+    }
+}
+
+impl From<Vector3> for Vector3i {
+    fn from(v: Vector3) -> Self {
+        Vector3i {
+            x: v.x as i64,
+            y: v.y as i64,
+            z: v.z as i64,
+        }
+    }
+}
+
+
+impl From<Vector4i> for Vector4 {
+    fn from(v: Vector4i) -> Self {
+        Vector4 {
+            x: v.x as f64,
+            y: v.y as f64,
+            z: v.z as f64,
+            w: v.w as f64,
+        }
+    }
+}
+
+impl From<Vector4> for Vector4i {
+    fn from(v: Vector4) -> Self {
+        Vector4i {
+            x: v.x as i64,
+            y: v.y as i64,
+            z: v.z as i64,
+            w: v.w as i64,
+        }
+    }
+}
